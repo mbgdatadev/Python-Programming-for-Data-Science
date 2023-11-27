a96c04ca682bc785ab1624dcf1b6d0fad5bde366
mbgdatadev <108692949+mbgdatadev@users.noreply.github.com>
2023-11-27 16:45:01 +0300

    Create setting_up_working_environment

---
+++ working_environment/setting_up_working_environment.py
@@ -0,0 +1,30 @@
+###############################################
+# Virtual Environment (Sanal Ortam)  ve (Package Managment) Paket Yönetimi
+###############################################
+#Aşağıdaki kodları terminale de çalıştuırınız. Eğer çalışmazsa Pycharm-Enviroment-Aktifleştirme.md dosyasındaki adımları izleyiniz. 
+# Sanal ortamların listelenmesi:
+# conda env list
+
+# Sanal ortam oluşturma:
+# conda create –n myenv
+
+# Sanal ortamı aktif etme:
+# conda activate myenv
+
+# Yüklü paketlerin listelenmesi:
+# conda list
+
+# Paket yükleme:
+# conda install numpy
+
+# Aynı anda birden fazla paket yükleme:
+# conda install numpy scipy pandas
+
+# Paket silme:
+# conda remove package_name
+
+# Belirli bir versiyona göre paket yükleme:
+# conda install numpy=1.20.1
+
+# Paket yükseltme:
+# conda upgrade numpy
