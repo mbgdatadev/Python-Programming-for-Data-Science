5a0aebbc47c290de2574ec537584b8143a544282
mbgdatadev <108692949+mbgdatadev@users.noreply.github.com>
2023-11-30 14:47:49 +0300

    Update python-loops.py

--- Python-Loops/python-loops.py
+++ Python-Loops/python-loops.py
@@ -174,3 +174,57 @@ def alternating_with_enumerate(string):
     print(new_string)
 
 alternating_with_enumerate("hi my name is john and i am learning python")
+
+
+
+#######################
+# Zip
+#######################
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+departments = ["mathematics", "statistics", "physics", "astronomy"]
+
+ages = [23, 30, 26, 22]
+
+list(zip(students, departments, ages))
+
+###############################################
+# lambda, map, filter, reduce
+###############################################
+
+def summer(a, b):
+    return a + b
+
+summer(1, 3) * 9
+
+new_sum = lambda a, b: a + b
+
+new_sum(4, 5)
+
+# map
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+def new_salary(x):
+    return x * 20 / 100 + x
+
+new_salary(5000)
+
+for salary in salaries:
+    print(new_salary(salary))
+
+list(map(new_salary, salaries))
+
+
+# del new_sum
+list(map(lambda x: x * 20 / 100 + x, salaries))
+list(map(lambda x: x ** 2 , salaries))
+
+# FILTER
+list_store = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list(filter(lambda x: x % 2 == 0, list_store))
+
+# REDUCE
+from functools import reduce
+list_store = [1, 2, 3, 4]
+reduce(lambda a, b: a + b, list_store)
